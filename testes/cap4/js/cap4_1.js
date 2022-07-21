4692a0220d2f25c2c177da3b71e9c51c074e9f3b
Eduardo Faria <eduardofaria.rj@gmail.com>
2022-07-20 21:28:01 -0300

    Inicio Tabuada, saindo do Atom e voltando pro VS Code!

---
+++ testes/cap4/js/cap4_1.js
@@ -0,0 +1,13 @@
+function mostrarTabuada() {
+  //DOMs
+  var inNumero = document.getElementById("inNumero");
+  var outTabuada = document.getElementById("outTabuada");
+
+  //variáveis
+  var numero = Number(inNumero.value);
+
+  //Geração da Tabuada
+  for (var i = 1; i < 11; i++) {
+    outTabuada.
+  }
+}
