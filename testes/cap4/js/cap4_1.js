3c9aa4ad5f83c493df657cb6fc7c3ebe2c1abdf6
Eduardo Faria <eduardofaria.rj@gmail.com>
2022-07-20 22:04:41 -0300

    Tabuada. DÚVIDA:  * POR QUE? tabuada = tabuada + numero + " x " + i + " = " + (numero * i) + "\n";  * NÃO DEVERIA SER: tabuada = numero + " x " + i + " = " + (numero * i) + "\n";

--- testes/cap4/js/cap4_1.js
+++ testes/cap4/js/cap4_1.js
@@ -5,9 +5,20 @@ function mostrarTabuada() {
 
   //variáveis
   var numero = Number(inNumero.value);
-
+  var tabuada = "";
   //Geração da Tabuada
-  for (var i = 1; i < 11; i++) {
-    outTabuada.
+  for (var i = 1; i <= 10; i++) {
+    tabuada = tabuada + numero + " x " + i + " = " + (numero * i) + "\n";
+/**
+ * DÚVIDA:
+ * POR QUE? tabuada = tabuada + numero + " x " + i + " = " + (numero * i) + "\n";
+ * NÃO DEVERIA SER: tabuada = numero + " x " + i + " = " + (numero * i) + "\n";
+ */
+
   }
+  outTabuada.textContent = tabuada;
 }
+
+//botão
+var btnExibir = document.getElementById("btnExibir");
+btnExibir.addEventListener("click", mostrarTabuada);
