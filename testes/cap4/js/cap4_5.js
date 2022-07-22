8fa4881f4932cce4bab09d4d9197a1d50893f85f
Eduardo Faria <eduardofaria.rj@gmail.com>
2022-07-22 01:44:08 -0300

    inicio exercicio 5.5

---
+++ testes/cap4/js/cap4_5.js
@@ -0,0 +1,18 @@
+// Acumuladores
+var numContontas = 0;
+var valorTotal = 0;
+var resposta = "";
+
+function adicionarContas() {
+    //DOMs
+    var inConta = document.getElementById("inConta");
+    var inValor = Number(document.getElementById("inValor"));
+    var outConta = document.getElementById("outConta");
+    var outTotal = document.getElementById("outTotal");
+
+    //Capturar Contas
+
+}
+
+var btnRegistrar = document.getElementById("btnRegistrar");
+btnRegistrar.addEventListener("click", adicionarContas);
