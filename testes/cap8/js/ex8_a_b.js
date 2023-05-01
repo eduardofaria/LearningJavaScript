f23e89e94fb336ad42dd9e763601a4c8c5a658a4
Eduardo Faria <eduardofaria.rj@gmail.com>
2023-05-01 19:37:02 -0300

    Adicionado comentários ao código

--- testes/cap8/js/ex8_a_b.js
+++ testes/cap8/js/ex8_a_b.js
@@ -3,13 +3,13 @@ acesso_verificar();
 const out_list = document.getElementById("out_list");
 const in_produto = document.getElementById("in_produto");
 
-function acesso_verificar(){
+function acesso_verificar(){ //verifica se houve acesso anterior
     (localStorage.getItem("acesso"))? numero_acesso = localStorage.getItem("acesso") : localStorage.setItem("acesso", 0);
     numero_acesso = localStorage.getItem("acesso");
     acesso_mensagem(numero_acesso);
 }
 
-function acesso_mensagem(contador){
+function acesso_mensagem(contador){ // atualiza a contagem e envia a mensagem de acordo
     contador++;
     let out_alert = document.getElementById("out_alert");
     if (contador == 1) {
@@ -20,18 +20,15 @@ function acesso_mensagem(contador){
     localStorage.setItem("acesso", contador);
 }
 
-
 const lista_salva = localStorage.getItem("lista") || null;
 let lista_compras = lista_salva != null? exibir_lista(lista_salva) : "";
-
-
-function gravar_lista(produto){
+function gravar_lista(produto){ // carrega lista salva na memória, ou a cria
     lista_compras += produto + ",";
     localStorage.setItem("lista", lista_compras);
     return exibir_lista(lista_compras);
 }
 
-function exibir_lista(itens){
+function exibir_lista(itens){ // exibe lista alfabéticamente na tela
     let lista = itens.split(",").sort();
     lista.shift();
     let lista_ordenada = `Produtos Adicionado\n-----------------------\n`;
@@ -41,7 +38,7 @@ function exibir_lista(itens){
     out_list.textContent = lista_ordenada;    
 }
 
-function limpar_lista(){
+function limpar_lista(){ // apaga tudo, de todos os lugares
     lista_compras = "";
     localStorage.removeItem("lista");
     out_list.textContent = "";
@@ -49,7 +46,7 @@ function limpar_lista(){
     in_produto.focus();
 }
 
-function add_item (){
+function add_item (){ // alimenta a lista item a item, verificando o preenchimento correto
     let produto = in_produto.value;
     if (produto == "" || !isNaN(produto)){
         alert("Informe um produto válido");
@@ -60,14 +57,12 @@ function add_item (){
     in_produto.focus();
 }
 
-
-
 const btn_add = document.getElementById("btn_add");
 btn_add.addEventListener("click", add_item);
 const btn_clear = document.getElementById("btn_clear");
 btn_clear.addEventListener("click", limpar_lista);
 
-in_produto.addEventListener("keydown", function(tecla){
+in_produto.addEventListener("keydown", function(tecla){ // permite usabilidade de salvar itens apertando o Enter
     if (tecla.key == "Enter"){
         add_item();
     }
