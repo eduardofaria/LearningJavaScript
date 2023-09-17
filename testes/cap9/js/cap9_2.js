4ad6f0c7d087e7f6ef5281942015ca33f2ea6026
Eduardo Faria <eduardofaria.rj@gmail.com>
2023-09-17 10:01:05 -0300

    Ex 9 concluído

--- testes/cap9/js/cap9_2.js
+++ testes/cap9/js/cap9_2.js
@@ -1,5 +1,7 @@
 let coins00_10, coins00_25, coins00_50, coins01_00;
+let outCoins = document.getElementById("outCoins");
 function newCoins(){
+    
     coins00_10 = Math.ceil(Math.random() * 5);
     coins00_25 = Math.ceil(Math.random() * 5);
     coins00_50 = Math.ceil(Math.random() * 5);
@@ -28,12 +30,24 @@ function verifyCoins(){
 }
 
 function showCoins(){
+    outCoins.innerHTML = "";
     newCoins();
-    console.log("Moedas de 0.10: " + coins00_10);
-    console.log("Moedas de 0.25: " + coins00_25);
-    console.log("Moedas de 0.50: " + coins00_50);
-    console.log("Moedas de 1 real: " + coins01_00);
-    console.log("Total: " + `${targetValue()}`)
+    createCoinsImages(coins00_10,"0_10.jpg","Moeda de 10 centavos","coin_0_10");
+    createCoinsImages(coins00_25,"0_25.jpg","Moeda de 25 centavos","coin_0_25");
+    createCoinsImages(coins00_50,"0_50.jpg","Moeda de 50 centavos","coin_0_50");
+    createCoinsImages(coins01_00,"1_00.jpg","Moeda de 1 real","coin_1_00");
+}
+
+function createCoinsImages(qtd, src, alt, css ){
+    for (let i = 1; i <= qtd; i++){
+        var newCoin = document.createElement("img");
+        newCoin.src = "img/" + src;
+        newCoin.alt = alt;
+        newCoin.css = css;
+        outCoins.appendChild(newCoin);
+    }
+    var newRow = document.createElement("br");
+    outCoins.appendChild(newRow);
 }
 
 let btnNewCoins = document.getElementById("btnNewCoins");
@@ -42,4 +56,4 @@ btnNewCoins.addEventListener("click", showCoins);
 let btnCheckCoins = document.getElementById("btnCheckCoins");
 btnCheckCoins.addEventListener("click", verifyCoins);
 
-newCoins();
+showCoins();
